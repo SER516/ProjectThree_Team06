abb1e01430bf518092de6f96d9313ffeb2c82fdf
Abhi <asmarat1@asu.edu>
2018-03-29 23:09:32 -0700

    Client view first commit

---
+++ src/client/view/MenuBar.java
@@ -0,0 +1,12 @@
+package client.view;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class MenuBar extends JFrame {
+    public MenuBar(){
+        JMenuBar memBar = new JMenuBar();
+        JMenu menu1 = new JMenu("File");
+    }
+
+}
