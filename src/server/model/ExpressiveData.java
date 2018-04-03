f24507565dd2f02e75aea1af7343bda3f2c0784f
Garv <gmathur1@asu.edu>
2018-04-02 20:33:01 -0700

    changed json

---
+++ src/server/model/ExpressiveData.java
@@ -0,0 +1,41 @@
+package server.model;
+
+public class ExpressiveData {
+	private float meditation = 0;
+	private float engagementBoredom = 0;
+	private float excitementShortTerm = 0;
+	private float frustation = 0;
+	private float  excitementLongTerm = 0;
+	
+	public float getMeditation() {
+		return meditation;
+	}
+	public void setMeditation(float meditation) {
+		this.meditation = meditation;
+	}
+	public float getEngagementBoredom() {
+		return engagementBoredom;
+	}
+	public void setEngagementBoredom(float engagementBoredom) {
+		this.engagementBoredom = engagementBoredom;
+	}
+	public float getExcitementShortTerm() {
+		return excitementShortTerm;
+	}
+	public void setExcitementShortTerm(float excitementShortTerm) {
+		this.excitementShortTerm = excitementShortTerm;
+	}
+	public float getFrustation() {
+		return frustation;
+	}
+	public void setFrustation(float frustation) {
+		this.frustation = frustation;
+	}
+	public float getExcitementLongTerm() {
+		return excitementLongTerm;
+	}
+	public void setExcitementLongTerm(float excitementLongTerm) {
+		this.excitementLongTerm = excitementLongTerm;
+	} 
+	
+}
