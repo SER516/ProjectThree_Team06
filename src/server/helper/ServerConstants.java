dfa02e254766bbd9e99e8459731f29f6ccce0221
Garv <gmathur1@asu.edu>
2018-04-02 21:09:35 -0700

    added initial clock

--- src/server/helper/ServerConstants.java
+++ src/server/helper/ServerConstants.java
@@ -3,5 +3,5 @@ package server.helper;
 public class ServerConstants {
 	public static boolean ONE_TIME_SEND = false;
 	public static boolean AUTO_RESET = false;
-	public static double STATE_INTERVAL= 2.5;
+	public static double STATE_INTERVAL= 0.25;
 }
