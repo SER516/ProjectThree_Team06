1a952d2864f2dbc58bde8d639986bf015d2a0dff
Rishabh27795 <rpmodi@asu.edu>
2018-04-05 23:14:09 -0700

    Refactored code

--- src/client/controller/ClientMainController.java
+++ src/client/controller/ClientMainController.java
@@ -8,32 +8,37 @@ import client.view.ClientFrame;
 import server.controller.ServerApplicationController;
 import server.controller.ServerMainController;
 
+/**
+ * Controller class to handle web socket connection between the client and the server
+ **/
 public class ClientMainController {
 
-	public static void main(String args[]) {
-		ClientDataSingleton.getInstance();
-		ClientFrame clientFrame = new ClientFrame();
-		setClientServerConnection(clientFrame);
-	}
+    public static void main(String args[]) {
+        ClientDataSingleton.getInstance();
+        ClientFrame clientFrame = new ClientFrame();
+        setClientServerConnection(clientFrame);
+    }
 
-	private static void setClientServerConnection(ClientFrame clientFrame) {
-		clientFrame.setConnectionListener(new ConnectionListener() {
-			@Override
-			public void startServer() {
-				WebSocketClientMain webSocketClientMain = new WebSocketClientMain();
-				if(!ClientDataSingleton.getInstance().isSessionMaintained()) {
-					webSocketClientMain.connectToServer();
-				}	
-			}
-			@Override
-			public void reconnectServer(String url) {
-				// TODO Auto-generated method stub
-				
-			}
-			@Override
-			public void initializeServer() {
-				new ServerApplicationController();
-			}
-		});	
-	}
+    private static void setClientServerConnection(ClientFrame clientFrame) {
+        clientFrame.setConnectionListener(new ConnectionListener() {
+            @Override
+            public void startServer() {
+                WebSocketClientMain webSocketClientMain = new WebSocketClientMain();
+                if (!ClientDataSingleton.getInstance().isSessionMaintained()) {
+                    webSocketClientMain.connectToServer();
+                }
+            }
+
+            @Override
+            public void reconnectServer(String url) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void initializeServer() {
+                new ServerApplicationController();
+            }
+        });
+    }
 }
