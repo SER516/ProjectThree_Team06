acf4094f0ae159acace59fcbcbbce4a1144e739c
salilmalik <salilmalik92@gmail.com>
2018-04-10 23:45:19 -0700

    Refactored DetectionPanel

--- src/server/view/DetectionPanel.java
+++ src/server/view/DetectionPanel.java
@@ -49,6 +49,17 @@ public class DetectionPanel extends JPanel implements ChangeListener, ActionList
 				ServerConstants.TEXT_FONT, null));
 		this.setBounds(11, 130, 474, 267);
 		this.setLayout(null);
+		timeDetectionPanelSetup();
+		upperFaceDetectionPanel();
+		lowerFaceDetectionPanelSetup();
+		eyeDetectionPanelSetup();
+		performanceMetricSetup();
+	}
+
+	/**
+	 * timeDetectionPanelSetup sets up the detection panel UI for the time
+	 */
+	private void timeDetectionPanelSetup() {
 		JLabel timeLabel = new JLabel(ServerConstants.TIME);
 		timeLabel.setFont(ServerConstants.TEXT_FONT);
 		timeLabel.setForeground(Color.WHITE);
@@ -67,59 +78,69 @@ public class DetectionPanel extends JPanel implements ChangeListener, ActionList
 		secondsLabel.setForeground(Color.WHITE);
 		secondsLabel.setBounds(113, 24, 64, 33);
 		this.add(secondsLabel);
+	}
 
+	/**
+	 * upperFaceDetectionPanel sets up the detection panel UI for the upper face
+	 */
+	private void upperFaceDetectionPanel() {
 		JLabel upperFaceLabel = new JLabel(ServerConstants.UPPER_FACE);
 		upperFaceLabel.setFont(ServerConstants.TEXT_FONT);
 		upperFaceLabel.setForeground(Color.WHITE);
 		upperFaceLabel.setBounds(14, 68, 139, 33);
 		this.add(upperFaceLabel);
-
 		String[] upperfaceItems = new String[] { ServerConstants.RAISE_BROW, ServerConstants.FURROW_BROW };
 		upperfaceComboBox = new JComboBox<>(upperfaceItems);
 		upperfaceComboBox.setBounds(14, 98, 139, 25);
 		upperfaceComboBox.addActionListener(this);
 		this.add(upperfaceComboBox);
-
 		upperfaceSpinner = new JSpinner();
 		upperfaceSpinner.setModel(new SpinnerNumberModel(0.00, 0.00, 1.00, 0.1));
 		upperfaceSpinner.setBackground(Color.WHITE);
 		upperfaceSpinner.setBounds(161, 97, 55, 25);
 		upperfaceSpinner.addChangeListener(this);
 		this.add(upperfaceSpinner);
+	}
 
+	/**
+	 * lowerFaceDetectionPanelSetup sets up the detection panel UI for the lower
+	 * face
+	 */
+	private void lowerFaceDetectionPanelSetup() {
 		JLabel lowerfaceLabel = new JLabel(ServerConstants.LOWERFACE);
 		lowerfaceLabel.setFont(ServerConstants.TEXT_FONT);
 		lowerfaceLabel.setForeground(Color.WHITE);
 		lowerfaceLabel.setBounds(250, 68, 139, 33);
 		this.add(lowerfaceLabel);
-
 		String[] lowerfaceItems = new String[] { ServerConstants.LAUGH, ServerConstants.CLENCH,
 				ServerConstants.SMIRK_LEFT, ServerConstants.SMIRK_RIGHT, ServerConstants.SMILE };
 		lowerfaceComboBox = new JComboBox<>(lowerfaceItems);
 		lowerfaceComboBox.setBounds(250, 98, 123, 25);
 		lowerfaceComboBox.addActionListener(this);
 		this.add(lowerfaceComboBox);
-
 		lowerfaceSpinner = new JSpinner();
 		lowerfaceSpinner.setModel(new SpinnerNumberModel(0.00, 0.00, 1.00, 0.1));
 		lowerfaceSpinner.setForeground(Color.WHITE);
 		lowerfaceSpinner.setBounds(383, 97, 52, 25);
 		lowerfaceSpinner.addChangeListener(this);
 		this.add(lowerfaceSpinner);
+	}
 
+	/**
+	 * eyeDetectionPanelSetup sets up the detection panel UI for the eye
+	 */
+	private void eyeDetectionPanelSetup() {
 		JLabel eyeLabel = new JLabel(ServerConstants.EYE);
 		eyeLabel.setFont(ServerConstants.TEXT_FONT);
 		eyeLabel.setForeground(Color.WHITE);
 		eyeLabel.setBounds(14, 134, 115, 33);
 		this.add(eyeLabel);
-
 		String[] eyeItems = new String[] { ServerConstants.BLINK, ServerConstants.WINK_LEFT, ServerConstants.WINK_RIGHT,
-				ServerConstants.LOOK_LEFT, ServerConstants.WINK_RIGHT };
+				ServerConstants.LOOK_LEFT, ServerConstants.LOOK_RIGHT };
 		eyeComboBox = new JComboBox<>(eyeItems);
 		eyeComboBox.setBounds(14, 163, 139, 25);
 		eyeComboBox.addActionListener(this);
 		this.add(eyeComboBox);
-
 		eyeActivateRadioButton = new JRadioButton(ServerConstants.ACTIVATE);
 		eyeActivateRadioButton.setFont(ServerConstants.TEXT_FONT);
 		eyeActivateRadioButton.setBackground(Color.GRAY);
@@ -127,7 +148,6 @@ public class DetectionPanel extends JPanel implements ChangeListener, ActionList
 		eyeActivateRadioButton.setBounds(185, 164, 95, 25);
 		eyeActivateRadioButton.addActionListener(this);
 		this.add(eyeActivateRadioButton);
-
 		eyeAutoResetCheckBox = new JCheckBox(ServerConstants.AUTO_RESETS);
 		eyeAutoResetCheckBox.setForeground(Color.WHITE);
 		eyeAutoResetCheckBox.setFont(ServerConstants.TEXT_FONT);
@@ -136,20 +156,24 @@ public class DetectionPanel extends JPanel implements ChangeListener, ActionList
 		eyeAutoResetCheckBox.addActionListener(this);
 		eyeAutoResetCheckBox.addActionListener(this);
 		this.add(eyeAutoResetCheckBox);
+	}
 
+	/**
+	 * performanceMetricSetup sets up the detection panel UI for the performance
+	 * metric
+	 */
+	private void performanceMetricSetup() {
 		JLabel performanceMetricsLabel = new JLabel(ServerConstants.PERFORMANCE_METRICS);
 		performanceMetricsLabel.setFont(ServerConstants.TEXT_FONT);
 		performanceMetricsLabel.setForeground(Color.WHITE);
 		performanceMetricsLabel.setBounds(14, 199, 131, 33);
 		this.add(performanceMetricsLabel);
-
 		String[] pfMetricItems = new String[] { ServerConstants.INTEREST, ServerConstants.ENGAGEMENT,
 				ServerConstants.STRESS, ServerConstants.RELAXATION, ServerConstants.EXCITEMENT, ServerConstants.FOCUS };
 		performanceMetricsComboBox = new JComboBox<>(pfMetricItems);
 		performanceMetricsComboBox.setBounds(14, 229, 139, 25);
 		performanceMetricsComboBox.addActionListener(this);
 		this.add(performanceMetricsComboBox);
-
 		performanceMetricsSpinner = new JSpinner();
 		performanceMetricsSpinner.setModel(new SpinnerNumberModel(0.00, 0.00, 1.00, 0.1));
 		performanceMetricsSpinner.setBounds(161, 229, 55, 25);
